e8e98e73780762c9bfd15c9b45d29a0f83f8b072
kyaching <kyochingmarma@gmail.com>
2022-11-08 03:23:37 +0600

    added about me section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -35,6 +35,31 @@ const Home = () => {
           <Button className="mx-auto">See All</Button>
         </Link>
       </div>
+      <section className="p-6 m-8">
+        <div className="container grid gap-6 mx-autotext-center lg:grid-cols-2 xl:grid-cols-5">
+          <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 bg-gray-900">
+            <span className="block mb-2 text-violet-400">Want to know?</span>
+            <h1 className="text-5xl font-extrabold text-gray-50">About Me</h1>
+            <p className="my-8">
+              <span className="font-medium text-gray-50">
+                Confident and dedicated photographer with experience in both
+                professional and freelance photography. Holds nearly 10 years of
+                working experience with great variety in order to tackle any
+                photography job quickly and effectively. Intimately familiar
+                taking high quality digital photographs, including framing,
+                selecting and setting up lighting, and determining advanced
+                shutter and lens options. Prioritizes communication on the job
+                to avoid errors.
+              </span>
+            </p>
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1516357231954-91487b459602?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
+            alt=""
+            className="object-cover w-full rounded-md xl:col-span-3 bg-gray-500"
+          />
+        </div>
+      </section>
     </div>
   );
 };
