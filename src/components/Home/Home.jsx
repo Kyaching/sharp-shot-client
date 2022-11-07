a663eff1782a1e504db40bfc946c221e40c77f30
kyaching <kyochingmarma@gmail.com>
2022-11-08 00:48:33 +0600

    load data from server

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,22 @@
 import { Button } from "flowbite-react";
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 import Slider from "../Header/Slider";
 import Services from "../Services/Services";
 
 const Home = () => {
+  const services = useLoaderData();
+  console.log(services.data);
   return (
     <div>
       <Slider />
       <div>
         <h1 className="text-5xl text-center">My Services</h1>
-        <Services />
+        <div className="grid grid-cols-3 gap-4 m-5">
+          {services.data.map((service) => (
+            <Services key={service._id} service={service} />
+          ))}
+        </div>
         <Button className="mx-auto">See All</Button>
       </div>
     </div>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,6 +10,7 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader: () => fetch("http://localhost:5000/services"),
       },
     ],
   },
