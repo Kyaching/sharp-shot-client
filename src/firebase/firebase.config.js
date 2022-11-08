3cef19b6954ae5d753aa2e8589cac8b9273086c2
kyaching <kyochingmarma@gmail.com>
2022-11-08 10:56:33 +0600

    added firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3EHCIAf58J-44ipwu8m6vRWcEE6eleEQ",
+  authDomain: "photography-review-687cc.firebaseapp.com",
+  projectId: "photography-review-687cc",
+  storageBucket: "photography-review-687cc.appspot.com",
+  messagingSenderId: "644912551558",
+  appId: "1:644912551558:web:e5d015e5a3da5d9ca4a307",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
